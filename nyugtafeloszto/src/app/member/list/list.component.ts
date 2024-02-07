353b102cd1657ddb3a0a008cd0c00c874b042fb8
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2024-02-07 20:30:07 +0100

    #31 Validate member form

--- nyugtafeloszto/src/app/member/list/list.component.ts
+++ nyugtafeloszto/src/app/member/list/list.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Member } from '../../shared/models/Member';
 import { MemberService } from 'src/app/shared/services/member.service';
-import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list',
@@ -14,7 +21,12 @@ export class ListComponent implements OnInit, OnDestroy {
   tableData?: Array<Member>;
   columnsToDisplay = ['name'];
   memberForm: FormGroup = new FormGroup({
-    name: new FormControl(''),
+    name: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(100),
+      Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/),
+      this.existenceValidator(),
+    ]),
   });
   user?: string | null;
   memberSubscription?: Subscription;
@@ -43,11 +55,34 @@ export class ListComponent implements OnInit, OnDestroy {
     if (!user) {
       return;
     }
+
+    const name: string = this.memberForm.controls['name'].value;
+
     const member: Member = {
       user: JSON.parse(user).uid,
-      name: this.memberForm.controls['name'].value,
+      name: name.trim(),
     };
     this.memberService.create(member);
     this.memberForm.controls['name'].setValue('');
+    this.memberForm.controls['name'].setErrors(null);
+  }
+
+  existenceValidator(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const value = control.value.trim();
+
+      if (!value) {
+        return null;
+      }
+
+      let exists = false;
+      this.tableData?.forEach((member) => {
+        if (member.name === value) {
+          exists = true;
+        }
+      });
+
+      return exists ? { exists: true } : null;
+    };
   }
 }

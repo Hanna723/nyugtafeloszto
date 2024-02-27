1e3f7a8081d4f8537de3e4ae6209d2e39c9f48c2
Somogyvári Hanna <h163907@stud.u-szeged.hu>
2024-02-27 21:15:53 +0100

    #37 Show user data on profile page

--- nyugtafeloszto/src/app/profile/preview/preview.component.ts
+++ nyugtafeloszto/src/app/profile/preview/preview.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { User } from 'src/app/shared/models/User';
+import { ImageService } from 'src/app/shared/services/image.service';
+import { UserService } from 'src/app/shared/services/user.service';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
-  styleUrls: ['./preview.component.scss']
+  styleUrls: ['./preview.component.scss'],
 })
-export class PreviewComponent {
+export class PreviewComponent implements OnInit, OnDestroy {
+  user?: User;
+  image?: string;
+  userSupscription?: Subscription;
+  imageSubscription?: Subscription;
 
+  constructor(
+    private userService: UserService,
+    private imageService: ImageService
+  ) {}
+
+  ngOnInit(): void {
+    const localUser = localStorage.getItem('user');
+
+    if (localUser) {
+      this.userSupscription = this.userService
+        .getById(JSON.parse(localUser).uid)
+        .subscribe((user) => {
+          this.user = user;
+
+          this.imageSubscription = this.imageService
+            .getImage(`/profile/${user?.profilePicture}`)
+            .subscribe((image) => {
+              this.image = image;
+            });
+        });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.userSupscription?.unsubscribe();
+  }
 }
--- nyugtafeloszto/src/app/profile/profile.module.ts
+++ nyugtafeloszto/src/app/profile/profile.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { PreviewComponent } from './preview/preview.component';
 import { EditComponent } from './edit/edit.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [PreviewComponent, EditComponent],
-  imports: [CommonModule, ProfileRoutingModule],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    MatCardModule,
+    MatButtonModule,
+    FlexLayoutModule,
+  ],
 })
 export class ProfileModule {}
